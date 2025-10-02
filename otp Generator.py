f19d9e6c6b34f2073aa2ce90e2d49d4558c7a926
rahman-ar12 <rahmanabdul02007@gmail.com>
2025-10-02 20:29:35 +0530

    Update otp Generator.py

--- otp Generator.py
+++ otp Generator.py
@@ -5,7 +5,6 @@ def generate_otp(length=4):
         raise ValueError("OTP length must be positive.")
     otp = ''.join([str(random.randint(0, 9)) for _ in range(length)])
     return otp
-
-# Example usage
+
 otp = generate_otp()  # default 4-digit OTP
 print("Your OTP is:", otp)
